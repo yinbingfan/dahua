5fab1bb7f2cdcdfb12ce952f8783c9c55e8c7446
bofan <1076798954@qq.com>
2020-04-01 14:12:59 +0800

    ''

--- src/components/home/banner.vue
+++ src/components/home/banner.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-      
+      <el-carousel indicator-position="outside">
+    <el-carousel-item v-for="item in 4" :key="item">
+      <h3>{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel>
   </div>
 </template>
 
